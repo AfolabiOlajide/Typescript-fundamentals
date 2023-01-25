3156138063cdaa44f68e78411f0d677c85f4318a
Afolabi Olajide Samuel <samolajide20@gmail.com>
2023-01-25 22:05:28 +0100

    finished learning about the different types in typscript

--- build/js/type.js
+++ build/js/type.js
@@ -3,7 +3,15 @@ let newName = "Dave";
 let greatestNumber;
 let isLoading;
 let undecided;
+let unionType;
+let demoCondition = true;
 newName = "Afolabi";
 greatestNumber = 777;
 isLoading = false;
 undecided = "Lets Put Something here";
+unionType = demoCondition ? 23 : "something else";
+const sum = (a, b) => {
+    return a + b;
+};
+// if you don't know the type of the data you can just mouse over and typescript will infer the type for you 
+let re = /\w+/g;
--- src/type.ts
+++ src/type.ts
@@ -2,8 +2,20 @@ let newName: string = "Dave";
 let greatestNumber: number;
 let isLoading: boolean
 let undecided: any;
+let unionType: string | number;
+
+let demoCondition: boolean = true
 
 newName = "Afolabi";
 greatestNumber = 777;
 isLoading = false;
-undecided = "Lets Put Something here"
+undecided = "Lets Put Something here";
+unionType =  demoCondition ? 23 : "something else";
+
+
+const sum = (a: string, b: number) => {
+    return a+b;
+}
+
+// if you don't know the type of the data you can just mouse over and typescript will infer the type for you 
+let re: RegExp  = /\w+/g; 
