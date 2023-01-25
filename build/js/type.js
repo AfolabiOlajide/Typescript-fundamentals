c3b241b2a93793ef217735f1848ec0a8e869ba4f
Afolabi Olajide Samuel <samolajide20@gmail.com>
2023-01-25 21:45:44 +0100

    started learning about types

---
+++ build/js/type.js
@@ -0,0 +1,9 @@
+"use strict";
+let newName = "Dave";
+let greatestNumber;
+let isLoading;
+let undecided;
+newName = "Afolabi";
+greatestNumber = 777;
+isLoading = false;
+undecided = "Lets Put Something here";
---
+++ src/type.ts
@@ -0,0 +1,9 @@
+let newName: string = "Dave";
+let greatestNumber: number;
+let isLoading: boolean
+let undecided: any;
+
+newName = "Afolabi";
+greatestNumber = 777;
+isLoading = false;
+undecided = "Lets Put Something here"
