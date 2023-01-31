3b84d8a239dcc025e7cdac025bba0d237919bb1b
Afolabi Olajide Samuel <samolajide20@gmail.com>
2023-01-31 17:22:36 +0100

    Concluded the function aspect of typescript

--- build/js/functions.js
+++ build/js/functions.js
@@ -28,3 +28,38 @@ const optional = (a, b, c) => {
 const defaultParams = (a, b, c = 5) => {
     return a + b + c;
 };
+// REST 
+const total = (...nums) => {
+    return nums.reduce((prev, curr) => prev + curr);
+};
+logmesg(total(1, 2, 3, 4)); // output ---> 10
+// NB:
+// When you use "REST" params you pass them as an array of whatever type you choose; 
+// The "never" type (not that popular)
+// This is for functions that explicitly throw errors
+const errorMsg = (msg) => {
+    throw new Error(msg);
+};
+// It also applies to functions that retuns an infinite loop
+// typescript automatically inferes the type when you hover on the function
+const infiniteExp = () => {
+    let i = 1;
+    while (true) {
+        i++;
+        if (i > 100)
+            break; // since typscript helps to infer the types and we notice that our function returns never 
+        // it is a good way to help you in your program to notice the error and fix it as we have 
+        // done here, now the infered typw is "void"
+    }
+};
+// Custom type guard
+const checkType = (value, type) => {
+    return typeof value === type ? true : false;
+};
+const numberOrString = (value) => {
+    if (checkType(value, "number"))
+        return "number";
+    if (checkType(value, "string"))
+        return "string";
+    return errorMsg("Cannor reach this point");
+};
--- src/functions.ts
+++ src/functions.ts
@@ -47,3 +47,42 @@ const optional = (a: number, b: number, c?: number): number => {
 const defaultParams = (a: number, b: number, c: number = 5): number => {
     return a + b + c;
 }
+
+// REST 
+const total = (...nums: number[]): number =>{
+    return nums.reduce((prev, curr) => prev + curr);
+}
+
+logmesg(total(1,2,3,4)); // output ---> 10
+// NB:
+// When you use "REST" params you pass them as an array of whatever type you choose; 
+
+
+// The "never" type (not that popular)
+// This is for functions that explicitly throw errors
+const errorMsg = (msg: string): never => {
+    throw new Error(msg);
+}
+
+// It also applies to functions that retuns an infinite loop
+// typescript automatically inferes the type when you hover on the function
+const infiniteExp = () => {
+    let i: number = 1
+    while (true){
+        i++;
+        if(i > 100) break // since typscript helps to infer the types and we notice that our function returns never 
+                          // it is a good way to help you in your program to notice the error and fix it as we have 
+                          // done here, now the infered typw is "void"
+    }
+}
+
+// Custom type guard
+const checkType = (value: any, type: string): boolean => {
+    return typeof value === type ? true : false;
+}
+
+const numberOrString = (value: number | string) : string => {
+    if(checkType(value, "number")) return "number";
+    if(checkType(value, "string")) return "string";
+    return errorMsg("Cannor reach this point");
+}
