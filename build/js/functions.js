0dd6d73413cc3559fb21af9953785e8f132859bc
Afolabi Olajide Samuel <samolajide20@gmail.com>
2023-01-31 15:09:09 +0100

    started out learning about functions

--- build/js/functions.js
+++ build/js/functions.js
@@ -1,2 +1,30 @@
 "use strict";
 let stringNumberArray = ["string", 233,];
+// Literal Types
+let userName;
+userName = "Excel";
+// Functions 
+const add = (a, b) => {
+    return a + b;
+};
+// void meaning none instead of using null or undefined
+const logmesg = (message) => {
+    console.log(message);
+};
+const multiplyNum = (a, b) => {
+    return a * b;
+};
+const addNum = function (a, b) {
+    return a + b;
+};
+// Optional parameters
+const optional = (a, b, c) => {
+    if (typeof c !== "undefined") {
+        return a + b + c;
+    }
+    return a + b;
+};
+// Default Parameters
+const defaultParams = (a, b, c = 5) => {
+    return a + b + c;
+};
--- src/functions.ts
+++ src/functions.ts
@@ -3,3 +3,47 @@ type StringOrNumber = string | number;
 type SNArray = StringOrNumber[];
 
 let stringNumberArray: SNArray = ["string", 233,];
+
+// Literal Types
+let userName : "John" | "Doe" | "Excel";
+
+userName = "Excel";
+
+// Functions 
+const add = (a :number, b: number): number => {
+    return a + b;
+}
+
+// void meaning none instead of using null or undefined
+const logmesg = (message: any): void => {
+    console.log(message)
+}
+
+// -----------------------------------------------------
+// using type for Type alias
+type MathFunction = ( a: number, b: number ) => number;
+// using interface for Type alias
+interface NewMathFunction {
+    ( a: number, b: number ) : number
+}
+
+const multiplyNum: MathFunction = (a, b) => {
+    return a*b;
+}
+
+const addNum: NewMathFunction = function(a, b) {
+    return a+b;
+}
+
+// Optional parameters
+const optional = (a: number, b: number, c?: number): number => {
+    if(typeof c !== "undefined"){
+        return a+b+c
+    }
+    return a+b
+}
+
+// Default Parameters
+const defaultParams = (a: number, b: number, c: number = 5): number => {
+    return a + b + c;
+}
